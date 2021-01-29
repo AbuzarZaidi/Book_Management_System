9883d93697624af8449dc4d0f14713fa96fbdf50
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2021-01-29 01:40:55 -0900

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -33,7 +33,7 @@ void book::getData()
 }
 
 void show_menu(book obj);
-
+void rename_remove(void);
 
 int main()
 {
@@ -62,7 +62,8 @@ void show_menu(book obj)
 	cout << " 1) Enter 1 for add Book Information: " << endl;
 	cout << " 2) Enter 2 for add Display Book Information: " << endl;
 	cout << " 3) Enter 3 for Search detail of Book: " << endl;
-	cout << " 4) Enter 4 for display detail of credit: " << endl;
+	cout << " 4) Enter 4 for Delete data of book by id: " << endl;
+	cout << " 5) Enter 5 for Update data of book by id: " << endl;
 	cout << "\n    Enter: ";
 	cin >> num;
 	switch (num)
@@ -70,28 +71,54 @@ void show_menu(book obj)
 	{
 	case 1:
 		//input_from_user();
+	{
 		obj.getData();
 		break;
+	}
 	case 2:
 		//output_from_user();
-
+	{
 		obj.showData(1, " ");
 		break;
+	}
 	case 3:
+	{
 		string s = " ";
 		string search = "";
 		cin.ignore();
-		cout << "Enter search: " << endl;
+		cout << "Enter ID to search: " << endl;
 		getline(cin, search);
 		search.append(s);
 		obj.showData(2, search);
-		//break;
-		/*case 4:
-
-		break;*/
+		break;
+	}
+	//end of case 3
+	case 4:
+	{
+		string d = " ";
+		string deleteData = "";
+		cin.ignore();
+		cout << "Enter ID to Delete: " << endl;
+		getline(cin, deleteData);
+		deleteData.append(d);
+		obj.showData(3, deleteData);
+		rename_remove();
+		break;
+	}
+	//end of case 4
+	   //=======  
+	case 5:
+		string Upd = " ";
+		string updateData = "";
+		cin.ignore();
+		cout << "Enter ID to To Update Data: " << endl;
+		getline(cin, updateData);
+		updateData.append(Upd);
+		obj.showData(4, updateData);
+		rename_remove();
+		break;
 
 	}
-
 }
 
 void book::showData(int comp, string searc)
@@ -102,6 +129,7 @@ void book::showData(int comp, string searc)
 
 	cout << setw(12) << "id" << setw(19) << "name" << setw(21) << "price" << endl;
 	string myText = "";
+	ofstream out1("temporary.txt", ios::app);
 	ifstream in("books_information.txt", ios::in);
 	while (getline(in, myText)) {
 		string id = "";
@@ -132,19 +160,77 @@ void book::showData(int comp, string searc)
 		}
 		else if (comp == 2)
 		{
-
-
-			if (id == searc) {
+			if (id == searc)
+			{
 
 				cout << setw(12) << id << setw(20) << name << setw(20) << price << endl;
 			}
 
 		}
-		else
+		else if (comp == 3)
 		{
-			cout << "Error! " << endl;
+			if (id == searc)
+			{
+				cout << setw(12) << id << setw(20) << name << setw(20) << price << endl;
+			}
+			else
+			{
+				out1 << " " << id << name << price << endl;
+			}
 		}
+		else if (comp == 4)
+		{
+			if (id == searc)
+			{
+				cout << setw(12) << id << setw(20) << name << setw(20) << price << endl;
+				cout << "This Bokk is going to be update." << endl;
+				int book_id = 0;
+				string book_name;
+				float book_price = 0.0;
+				cout << "Enter Book id: " << endl;
+				cin >> book_id;
+				cin.ignore();
+				cout << "Enter name of book: " << endl;
+				getline(cin, book_name);
+				cout << "Enter Price of book: " << endl;
+				cin >> book_price;
 
+				out1 << " " << book_id << " " << book_name << " " << book_price << endl;
+				continue;
+			}
+			out1 << " " << id << name << price << endl;
+		}
 	}
+	out1.close();
 	in.close();
 }
+
+
+//rename/remove
+void rename_remove(void) {
+	/*	Deletes the file if exists */
+	char filename[] = "books_information.txt";
+	if (remove(filename) != 0)
+	{
+		perror("File deletion failed");
+	}
+	else
+	{
+		cout << "File deleted successfully" << endl;
+	}
+
+	//rename
+	char oldname[] = "temporary.txt";
+	char newname[] = "books_information.txt";
+
+	if (rename(oldname, newname) != 0)
+
+	{
+		perror("Error renaming file");
+	}
+	else
+	{
+		cout << "Process Complete Successfully!" << endl;
+	}
+}
+//======================================================The End=================================
